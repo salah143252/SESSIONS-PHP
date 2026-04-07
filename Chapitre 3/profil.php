202d91e3c57746d9c4bd764f8eb51b067abdb750
salah143252 <salahmassaad04@gmail.com>
2026-04-07 10:44:51 +0100

    Delete

--- Chapitre 3/profil.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['utilisateur'])) {
-    header('Location: login.php');
-    exit;
-}
-echo "<h1>Bienvenue " . $_SESSION['utilisateur'] . " !</h1>";
-echo "<a href='logout.php'>Se déconnecter</a>";
-?>
