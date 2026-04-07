eadac7a8eb9899cf38ff447c21b6b9edb23f8f13
salah143252 <salahmassaad04@gmail.com>
2026-04-07 10:54:03 +0100

    Delete

--- Chapitre 5/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['user'])) {
-    header('Location: login.php');
-    exit;
-}
-echo "<h1>Bienvenue " . $_SESSION['user'] . " !</h1>";
-echo "<a href='logout.php'>Se déconnecter</a>";
-?>
